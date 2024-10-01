87f7f2eb7ca8bdf3f70ce7b4ce7055ebc23d8ec8
Pansilu Ruvinsara <101954975+Ruvinsara23@users.noreply.github.com>
2024-10-01 10:25:40 +0530

    Add mongoDB to NASA Project

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,28 @@
 const express =require('express');
+const mongoose=require('mongoose');
 const http = require('http');
 const app =require('./app');
 const PORT=process.env.PORT || 8000;
+const MONGO_URL='mongodb+srv://nasa-api:nasa-api@nasacluster.bap9i.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster'
 const server = http.createServer(app)
 const { loadPlanetsData } =require('./models/planets.models');
 
+
+mongoose.connection.on('open',()=>{
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error',(err)=>{
+ console.error(err)
+})
 async function startServer() {
-    await loadPlanetsData;
+    await mongoose.connect(MONGO_URL, {
+useNewUrlParser: true,
+        // useFindAndModify:false,
+        // useCreateIndex:true,
+        useUnifiedTopology: true,
+    })
+    await loadPlanetsData();
     server.listen(PORT,()=>{
         console.log(`Listening on PORT ${PORT}...`)
     }) 
