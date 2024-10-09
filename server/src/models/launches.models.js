70ede3db48783330bbedfea2f2297fddb667c2b5
Pansilu Ruvinsara <101954975+Ruvinsara23@users.noreply.github.com>
2024-10-09 23:53:33 +0530

    add launches to database

--- server/src/models/launches.models.js
+++ server/src/models/launches.models.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require('./launches.mongo')
+const planets=require('./planets.mongo')
 
  const launches =new Map();
 
@@ -8,7 +9,7 @@ const launch={
     mission:'Kepler Exploration x',
     rocket:'Explorer Is1',
     launchDate: new Date('December 27,2030'),
-    target:'Kepler-442 b',
+    target:'Kepler',
     customer:['ZTM','NASA'],
     upcoming:true,
     success:true,
@@ -27,7 +28,23 @@ async function getAllLaunches(){
     return await launchesDatabase.find({},{'_id':0,'__v':0})  ;
 }
 
+async function getLatestFlightNumber(){
+    const latestLaunch=await launchesDatabase.findOne()
+    .sort('-flightNumber')
+    if(!latestLaunch){
+        return DEFAULT_FLIGHT_NUMBER
+    }
+  return latestLaunch.flightNumber
+}
+
 async function saveLaunch(launch){
+    const planet = await planets.findOne({
+        keplerName:launch.target,
+    })
+    if(!planet){
+        throw new Error ('No matching planet found  ')
+
+    }
     await launchesDatabase.updateOne({
         flightNumber: launch.flightNumber,     
     },launch,{
@@ -35,18 +52,31 @@ async function saveLaunch(launch){
     })
 }
 
-function addNewLaunch(launch){
-    latestFlightNumber++;
-    const newLaunch = {
-        ...launch,
-        flightNumber: latestFlightNumber,
-        success: true,
-        upcoming: true,
-        customer: ['ZTMN', 'NBNN'],
-    };
-    launches.set(newLaunch.flightNumber, newLaunch);
+async function scheduleNewLaunch(){
+    const newFlightNumber= await getLatestFlightNumber()+1;
 
+    const newLaunch=Object.assign(launch,{
+        success:true,
+        upcoming:true,
+        customer:['Zero to mastery','NASA'],
+        flightnumber:newFlightNumber,
+    })
+
+    await saveLaunch(newLaunch)
 }
+
+// function addNewLaunch(launch){
+//     latestFlightNumber++;
+//     const newLaunch = {
+//         ...launch,
+//         flightNumber: latestFlightNumber,
+//         success: true,
+//         upcoming: true,
+//         customer: ['ZTMN', 'NBNN'],
+//     };
+//     launches.set(newLaunch.flightNumber, newLaunch);
+
+// }
 
 function abortLaunchById(launchId){
      const aborted= launches.get(launchId);
@@ -60,6 +90,7 @@ module.exports={
     existsLaunchWithId,
     getAllLaunches,
     addNewLaunch,
+    scheduleNewLaunch,
     abortLaunchById,
 
 }
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const {getAllLaunches,addNewLaunch,existsLaunchWithId,abortLaunchById}=require('../../models/launches.models');
+const {getAllLaunches,scheduleNewLaunch,existsLaunchWithId,abortLaunchById}=require('../../models/launches.models');
 
 
 
@@ -6,7 +6,7 @@ async function httpGetAllLaunches(req, res) {
     return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
     if (!launch.mission || !launch. rocket
@@ -17,7 +17,7 @@ function httpAddNewLaunch(req, res) {
        })
     }
     launch.launchDate = new Date(launch.launchDate);
-    addNewLaunch(launch);
+   await scheduleNewLaunch(launch);
     return res.status(201).json(launch);
 }
 
